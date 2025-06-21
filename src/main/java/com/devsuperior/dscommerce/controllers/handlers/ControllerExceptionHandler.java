535e81bedead93a9bac06ce1b415cdd3eaf1b49e
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2025-06-21 18:22:38 -0300

    Exception Handler for beam validation

--- src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscommerce.controllers.handlers;
 
 import com.devsuperior.dscommerce.dto.CustomError;
+import com.devsuperior.dscommerce.dto.ValidationError;
 import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.Instant;
@@ -24,6 +27,17 @@ public class ControllerExceptionHandler {//classe necessária para interceptar o
     public ResponseEntity<CustomError> dataBase(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(),status.value(), e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(),status.value(),"Dados invalidos",request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()){   //Caminho da classe de exceção que captura os valores do Bean Validation
+            err.addErrors(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
---
+++ src/main/java/com/devsuperior/dscommerce/dto/ValidationError.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscommerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError{
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public List<FieldMessage> getErros() {
+        return errors;
+    }
+
+    public  void addErrors(String fieldName, String message){
+        errors.add(new FieldMessage(fieldName, message));
+
+    }
+}
