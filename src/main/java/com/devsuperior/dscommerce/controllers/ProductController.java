ec4884d8b7d2a44e39f37e92934d4a2caaebb532
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2025-09-19 01:35:44 -0300

    Limitando acesso a endpoints por  Role de usuario
    Não esquecer de definir bearer token em postman para endoints que foram assinados

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,19 +1,27 @@
 package com.devsuperior.dscommerce.controllers;
 
-import com.devsuperior.dscommerce.dto.CustomError;
-import com.devsuperior.dscommerce.dto.ProductDTO;
-import com.devsuperior.dscommerce.services.ProductService;
-import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
-import jakarta.validation.Valid;
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.time.Instant;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value ="/products")
@@ -22,6 +30,7 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id ){
             ProductDTO dto =  service.findById(id);
@@ -36,6 +45,7 @@ public class ProductController {
     }
 
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
         dto =service.insert(dto);
@@ -46,6 +56,7 @@ public class ProductController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id  ,@Valid @RequestBody ProductDTO dto){
         dto =service.update(id,dto);
@@ -53,6 +64,7 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
