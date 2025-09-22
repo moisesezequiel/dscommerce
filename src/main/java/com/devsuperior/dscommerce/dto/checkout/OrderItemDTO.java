b25d1fe0719794988a91e3d2428a73ff65a3a9f7
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2025-09-22 02:34:47 -0300

    imcremento de campo de imagem para o pedido

--- src/main/java/com/devsuperior/dscommerce/dto/checkout/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/checkout/OrderItemDTO.java
@@ -8,18 +8,20 @@ public class OrderItemDTO {
 	private String name;
 	private Double price;
 	private Integer quantity;
+	private String imgUrl;
 	
 	
 	public OrderItemDTO() {
 		
 	}
 	
-	public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+	public OrderItemDTO(Long productId, String name, Double price, Integer quantity,String imgUrl) {
 		super();
 		this.productId = productId;
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+		this.imgUrl =imgUrl;
 	}
 	
 	public OrderItemDTO(OrderItem entity) {
@@ -28,6 +30,7 @@ public class OrderItemDTO {
 		this.name = entity.getProduct().getName();
 		this.price = entity.getPrice();
 		this.quantity = entity.getQuantity();
+		this.imgUrl = entity.getProduct().getImgUrl();
 	}
 
 	public Long getProductId() {
@@ -49,5 +52,11 @@ public class OrderItemDTO {
 	public Double getSubtotal() {
 		return price * quantity;
 	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	
+	
 	
 }
